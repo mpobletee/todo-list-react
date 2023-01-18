608043c0e9af2d6eb31d4aed4187a7e097f731db
mpobletee <mpoblete95@gmail.com>
2023-01-18 05:43:49 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,51 @@
-import React from "react";
+import React, { useState } from 'react';
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [tasks, setTasks] = useState([]);
+	const [newTask, setNewTask] = useState('');
+	const [hovered, setHovered] = useState('');
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		if (newTask) {
+			setTasks([...tasks, newTask]);
+			setNewTask('');
+		}
+	}
+
+	const handleKeyPress = (event) => {
+		if (event.key === 'Enter') {
+			handleSubmit(event);
+		}
+	}
+
+	const handleDelete = (index) => {
+		setTasks(tasks.filter((task, i) => i !== index));
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className='container parent'>
+			<form className="form-group" onSubmit={handleSubmit}>
+				<input type="text" value={newTask} onChange={e => setNewTask(e.target.value)} onKeyDown={handleKeyPress} placeholder="Add a task" />
+			</form>
+			{tasks.length > 0 ? (
+				<ul>
+					{tasks.map((task, index) => (
+						
+						<li key={index} onMouseEnter={() => setHovered(index)} onMouseLeave={() => setHovered(-1)}>
+							<i class="fa-solid fa-star icono"></i> {task}
+							<a className={index === hovered ? 'show' : 'd-none'} onClick={() => handleDelete(index)}><i className="fa-regular fa-trash-can"></i></a>
+						</li>
+					))}
+				</ul>
+			) : (
+				<p>No tasks, add a task</p>
+			)}
 		</div>
 	);
-};
+}
 
 export default Home;
